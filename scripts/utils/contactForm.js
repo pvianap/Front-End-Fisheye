2a4fdbf72b324227dc8f06a6f02d92ba484fe7b5
pvianap <pedro.v.parente@gmail.com>
2022-08-01 15:59:47 +0200

    Refactoring contact modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -5,6 +5,7 @@ class ContactModal {
     this.$modal = document.querySelector('#contact_modal');
     this.$contactButton = document.querySelector('.contact_button');
     this.$closeButton = document.querySelector('.modal header img');
+    this.$submitButton = document.querySelector('.submitButton');
     this.addListener();
   }
 
@@ -28,60 +29,59 @@ class ContactModal {
     name.innerText = photographer.name;
   }
 
+  submitInputs() {
+    const name = document.querySelector('#name').value;
+    const last = document.querySelector('#last').value;
+    const email = document.querySelector('#email').value;
+    const message = document.querySelector('#message').value;
+    const mailContent = [{ name, last, email, message }];
+    console.log(mailContent);
+    return mailContent;
+  }
+
+  cleanInputs() {
+    document.querySelectorAll('input').forEach((e) => {
+      e.value = '';
+    });
+  }
+
+  formSubmit() {
+    const modal = document.querySelector('.modal');
+    modal.addEventListener('submit', (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      this.submitInputs();
+      this.cleanInputs();
+      this.closeModal();
+    });
+  }
+
   addListener() {
     this.$contactButton.addEventListener('click', (e) => {
       this.openModal();
     });
+
     this.$closeButton.addEventListener('click', (e) => {
       this.closeModal();
+    });
+    document.addEventListener('keyup', (e) => {
+      if (e.isComposing || e.keyCode === 27) {
+        return this.closeModal();
+      }
+    });
+    this.$modal.addEventListener('submit', (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      this.submitInputs();
+      this.cleanInputs();
+      this.closeModal();
+    });
+    this.$submitButton.addEventListener('keyup', (e) => {
+      if (e.isComposing || e.keyCode === 13) {
+        return this.closeModal();
+      }
     });
   }
 }
 
 new ContactModal();
-
-// function displayModal() {
-//   const modal = document.getElementById('contact_modal');
-//   modal.style.display = 'block';
-
-//   contactHeaderDOM();
-// }
-
-// function closeModal() {
-//   const modal = document.getElementById('contact_modal');
-//   modal.style.display = 'none';
-// }
-
-// function contactHeaderDOM() {
-//   const name = document.querySelector('.photographerName');
-//   name.innerText = photographer.name;
-// }
-
-function submitInputs() {
-  const name = document.querySelector('#name').value;
-  const last = document.querySelector('#last').value;
-  const email = document.querySelector('#email').value;
-  const message = document.querySelector('#message').value;
-  const mailContent = [{ name, last, email, message }];
-  console.log(mailContent);
-  return mailContent;
-}
-
-function cleanInputs() {
-  document.querySelectorAll('input').forEach((e) => {
-    e.value = '';
-  });
-}
-
-function formSubmit() {
-  const modal = document.querySelector('.modal');
-  modal.addEventListener('submit', (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    submitInputs();
-    cleanInputs();
-    closeModal();
-  });
-}
-
-formSubmit();
